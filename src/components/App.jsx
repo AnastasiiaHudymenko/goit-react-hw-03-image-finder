e91864bc3181b0e4ccca6dc1a780304ed5349f40
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-02-10 18:31:56 +0200

    add component form and list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+
+const API_KEY = '31544306-98c50720df0af9d375614fc16';
+const BASE_URL =
+  'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +15,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Searchbar />
+      <ImageGallery />
     </div>
   );
 };
