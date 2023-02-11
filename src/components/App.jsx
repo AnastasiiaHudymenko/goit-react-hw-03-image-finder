b85df1159c46388b654f8fac18a733525975ad3d
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-02-11 11:57:01 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 
-const API_KEY = '31544306-98c50720df0af9d375614fc16';
-const BASE_URL =
-  'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12';
+// const API_KEY = '31544306-98c50720df0af9d375614fc16';
+// const BASE_URL =
+//   'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12';
 
 export class App extends React.Component {
   state = {
